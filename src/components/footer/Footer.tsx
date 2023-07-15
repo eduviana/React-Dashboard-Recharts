3c773240c541a9c7a479d8604644588589be8893
edu <eduardoviana83@gmail.com>
2023-07-15 18:34:55 -0300

    first commit

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,11 @@
+import "./footer.scss";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <span>eduadmin</span>
+      <span>© Edu Dev Admin Dashboard</span>
+    </div>
+  )
+}
+export default Footer
